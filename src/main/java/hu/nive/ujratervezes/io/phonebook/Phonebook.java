df1b968428acd747ae27fd82c243946e2b26fc81
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-04-29 12:01:07 +0200

    refact

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -7,15 +7,6 @@ import java.util.Map;
 
 public class Phonebook {
 
-    //TODO A paraméterben kapott Mapben a kulcsok nevek, az értékek pedig telefonszámok.
-    // Írd ki az összes kulcs-érték párt az outputként megadott címre, minden kulcs-érték párt egy új sorba, az alábbi szintaxisban:
-    //
-    //név: telefonszám
-    //
-    //A kiírás sorrendje a fájlba nem fontos, de a Map összes elemét pontosan egyszer írd ki a fájlba!
-    //
-    //Ha a metódus akármelyik paraméternek null-t kapna, akkor dobj IllegalArgumentException-t!
-
     public void exportPhonebook(Map<String, String> contacts, String output) {
 
         if (contacts == null || output == null) {
@@ -23,13 +14,11 @@ public class Phonebook {
         }
 
         File file = new File(output);
-        try {
-            file.delete();
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        cleanFile(file);
+        writeFile(contacts, file);
+    }
 
+    private void writeFile(Map<String, String> contacts, File file) {
         try (FileWriter fileWriter = new FileWriter(file, true)){
             for (Map.Entry<String, String> entry : contacts.entrySet() ) {
                 String name = entry.getKey();
@@ -39,6 +28,14 @@ public class Phonebook {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private void cleanFile(File file) {
+        try {
+            file.delete();
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
