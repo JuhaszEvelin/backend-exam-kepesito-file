0db5945e97bef049619f9ed5a759f73b766c5482
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-04-29 11:43:18 +0200

    2 test passed

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,24 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.util.Map;
+
+public class Phonebook {
+
+    //TODO A paraméterben kapott Mapben a kulcsok nevek, az értékek pedig telefonszámok.
+    // Írd ki az összes kulcs-érték párt az outputként megadott címre, minden kulcs-érték párt egy új sorba, az alábbi szintaxisban:
+    //
+    //név: telefonszám
+    //
+    //A kiírás sorrendje a fájlba nem fontos, de a Map összes elemét pontosan egyszer írd ki a fájlba!
+    //
+    //Ha a metódus akármelyik paraméternek null-t kapna, akkor dobj IllegalArgumentException-t!
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+
+    }
+}
